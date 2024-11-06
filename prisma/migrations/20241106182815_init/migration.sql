7ce98afdebe38be19d5d8349e645f9d0d23092c6
drypzz <fdfjonfmd@gmail.com>
2024-11-06 15:56:38 -0300

    feat: Fixed

--- prisma/migrations/20241106182815_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "cost" REAL NOT NULL,
-    "deadline" DATETIME NOT NULL,
-    "order" INTEGER NOT NULL
-);
